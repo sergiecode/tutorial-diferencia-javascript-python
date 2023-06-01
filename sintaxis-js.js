bd227e9baa7d7277be4f13231c99b762048f46f3
SnSDk <sergiosnsdk@gmail.com>
2023-06-01 11:05:22 -0300

    diferencia-js-y-py

---
+++ sintaxis-js.js
@@ -0,0 +1,17 @@
+
+// ¿Me alcanza el dinero para comprar mi curso de programación web?
+
+let miDinero = 50
+let cursosWeb = 100
+let sergieCodeYoutube = "gratis"
+
+if (miDinero > cursosWeb) {
+    console.log("Te alcanza para los cursos")
+}
+else {
+    console.log("No te alcanza para pagar cursos")
+    if (sergieCodeYoutube === "gratis") {
+        console.log("pero pronto Sergie sacará cursos gratis en Youtube")
+    }
+}
+
---
+++ sintaxis-python.py
@@ -0,0 +1,12 @@
+#¿Me alcanza el dinero para comprar mi curso de programación web?
+
+mi_dinero = 50
+cursos_web = 100
+sergie_code_youtube = "gratis"
+
+if mi_dinero > cursos_web:
+ print("Te alcanza para los cursos")  
+else:
+ print("¡No te alcanza para pagar cursos")  
+ if sergie_code_youtube == "gratis":
+  print("Pero pronto Sergie sacará cursos gratis en Youtube")
